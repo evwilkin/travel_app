c49193a4552949aed7ae7faedc7464ed59b91d09
evwilkin <evan.wilkinson@gmail.com>
2016-02-25 13:52:56 -0800

    Can add categories to existing wishlist

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -6,8 +6,11 @@ router.get('/', function(req, res) {
 	var userId = req.currentUser.id;
 	if (req.currentUser.username) {
 		db.user.findById(userId).then(function(user) {  //identify current user
-			user.getWishlists().then(function(lists) {		//find all user's wishlists
+			user.getWishlists({
+				include: [db.category, db.attraction]
+			}).then(function(lists) {		//find all user's wishlists
 				res.render('wishlist/index', {lists: lists});
+				// res.send(lists);
 			});
 		});
 	} else {
@@ -49,8 +52,19 @@ router.get('/new', function(req, res) {
 router.get('/:id', function(req, res) {
 	var listId = req.params.id;
 	var userId = req.currentUser.id;
-	db.wishlist.findById(listId).then(function(list) {
-		if (list) {
+	db.wishlist.findById(listId
+		// , {
+		// 		include: [db.category, db.attraction]
+		// 	}
+			).then(function(list) {
+
+				list.getCategories({
+					include: [db.attraction]
+				}).then(function(categories) {
+					res.render('wishlist/show', {list: list, categories: categories})
+				});
+
+		/*if (list) {
 			if (list.userId === userId) {
 				console.log("List id:"+list.userId+" UserId: "+userId)
 				res.render('wishlist/show', {list: list});
@@ -59,23 +73,39 @@ router.get('/:id', function(req, res) {
 			}
 		} else {
 			res.send('Page not found.');
-		}
+		}*/
 	});
 });
 
-/*router.post('/:id', function(req, res) {
+router.post('/:id', function(req, res) {
 	var listId = req.params.id;
 	var currentUser = req.currentUser.id;
-	var newItem = req.body.item;
+	var newCategory = req.body.newCategory;
+	var newItem = req.body.newItem;
 	db.wishlist.findOne({
 		where: {
 			id: listId
 		}
 	}).then(function(list) {
-		list.append
-	})
-	}
-})*/
+		if (newCategory) {
+			db.category.findOrCreate({
+				where: {
+					wishlistId: list.id,
+					categoryname: newCategory 
+				}
+			}).spread(function(category, created) {
+				if (created) {
+					res.redirect("/wishlist/"+listId);
+				} else {
+					res.send("Category already exists");
+				}
+			});
+		}
+	});
+	// res.send("New Item: "+newItem+" in list: "+listId);
+});
+
+
 
 
 
